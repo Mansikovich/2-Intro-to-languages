be30391ea4b390f26cbd8e2507cac6853eb04a1c
Mansur Dagaev <mans.4e4en@mail.ru>
2023-01-08 02:46:46 +0300

    second commit

--- Task 10.cs
+++ Task 10.cs
@@ -7,18 +7,11 @@ class Program
 static void Main(string[] args)
 {
 
-Console.Write("Введите число: ");
-int n = Convert.ToInt32(Console.ReadLine());
-int x = 1;
-for(int i = 0; i < n; i++)
-{
-    if (x % 2 == 0)
-    {
-        Console.WriteLine(x);
-    }
-    x++;
+Console.Write("Введи трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(number);
+Console.WriteLine("вторая цифра этого числа -> "+stringNumber[1]);
 }
     
-}
 }
 }
